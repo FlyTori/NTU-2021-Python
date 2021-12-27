8fce5be88055c9e9d60822618c57eeac4f8c83c2
TORI CHEN <jiansheau1357@gmail.com>
2021-12-27 19:02:05 +0900

    heroku test

--- app.py
+++ app.py
@@ -7,7 +7,36 @@ import pandas as pd
 import requests #如果最後沒有使用alpha vantage可刪除
 from datetime import datetime
 
-app = Flask(__name__) ###__name__ 代表目前執行的模組
+#建立Application物件，設定靜態檔案的路徑處理
+app = Flask(__name__, static_folder="static", static_url_path="/")
+
+
+def getCompanyPrice(company, start_time, end_time):
+    # 使用yfinance抓取公司股價。由於一家公司與多家公司回傳格式不同，為了後續處理方便，統一單次只抓取單家公司
+    data = yf.download(company, start=start_time, end=end_time)
+
+    #原始資料有很多欄位，抓取Adj Close欄位，並從Pandas Series格式轉成list
+    company_adj_close = data["Adj Close"].tolist() 
+    return company_adj_close
+
+# 利用datetime套件計算相差天數，計算IRR用
+def daysCount(start_time, end_time):
+    start = datetime.strptime(start_time, "%Y-%m-%d")
+    end = datetime.strptime(end_time, "%Y-%m-%d")
+    diff = end.date() - start.date()
+    return(diff.days)
+
+# 使用 GET 方法處理路徑 / 的對應函式
+@app.route("/")  #路徑為/
+def index():
+    return render_template("index.html")  #連到index.html
+
+
+
+
+
+
+
 
 @app.route("/") 
 def index():
